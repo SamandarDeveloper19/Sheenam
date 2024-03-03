a8e69663a43716365d1c2ce2c4f057ad89b2f49c
SamandarDeveloper19 <mamadiyorovsamandar19@gmail.com>
2024-03-03 23:43:15 +0500

    ShouldAddGuestAsync -> FAIL

---
+++ Sheenam.Api/Services/Foundations/Guests/IGuestService.cs
@@ -0,0 +1,9 @@
+﻿using Sheenam.Api.Models.Foundations.Guests;
+
+namespace Sheenam.Api.Services.Foundations.Guests
+{
+    public interface IGuestService
+    {
+        ValueTask<Guest> AddGuestAsync(Guest guest);
+    }
+}
