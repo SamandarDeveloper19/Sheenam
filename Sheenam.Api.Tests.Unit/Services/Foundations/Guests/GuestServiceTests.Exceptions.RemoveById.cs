380805b83a9dd88ed56ba06ba0f54c2e1cc83b60
SamandarDeveloper19 <mamadiyorovsamandar19@gmail.com>
2024-04-27 10:53:12 +0500

    ShouldThrowServiceExceptionOnRemoveByIdIfServiceErrorOccursAndLogItAsync -> PASS

--- Sheenam.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.Exceptions.RemoveById.cs
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.Exceptions.RemoveById.cs
@@ -43,5 +43,43 @@ namespace Sheenam.Api.Tests.Unit.Services.Foundations.Guests
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnRemoveByIdIfServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid inputGuestId = Guid.NewGuid();
+            var serviceException = new Exception();
+
+            var failedGuestServiceException =
+                new FailedGuestServiceException(serviceException);
+
+            var expectedGuestServiceException =
+                new GuestServiceException(failedGuestServiceException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectGuestByIdAsync(It.IsAny<Guid>()))
+                .ThrowsAsync(serviceException);
+
+            // when
+            ValueTask<Guest> removeGuestById =
+                this.guestService.RemoveGuestByIdAsync(inputGuestId);
+
+            // then
+            await Assert.ThrowsAsync<GuestServiceException>(() =>
+                removeGuestById.AsTask());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectGuestByIdAsync(It.IsAny<Guid>()),
+                Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedGuestServiceException))),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
